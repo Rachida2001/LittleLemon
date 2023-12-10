b8de6c6c615541efd410e465ca94de3c05c2c910
Rachida <Rachida@gmail.com>
2023-12-10 13:46:07 -0600

    created the database and update the code

--- littlelemon1/restaurant/models.py
+++ littlelemon1/restaurant/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+class Menu(models.Model):
+    # Define the fields for the Menu model
+    ID = models.IntegerField(primary_key=True)
+    Title = models.CharField(max_length=255)
+    Price = models.DecimalField(max_digits=10, decimal_places=2)
+    Inventory = models.IntegerField()
+  
+
+    def __str__(self):
+        return self.Title  
+
+class Booking(models.Model):
+    # Define the fields for the Booking model
+    ID = models.IntegerField(primary_key=True)
+    Name = models.CharField(max_length=255)
+    No_of_guests = models.IntegerField()
+    BookingDate = models.DateTimeField(auto_now_add=True)
+   
+
+    def __str__(self):
+        return f"Booking for {self.No_of_guests} people on {self.BookingDate}" # Custom string representation
